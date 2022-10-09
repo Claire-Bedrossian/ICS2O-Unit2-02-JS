6e7f2211d695ba3726a6376aa6d25cf1955157dd
Claire-Bedrossian <Claire-Bedrossian@users.noreply.github.com>
2022-10-09 14:12:28 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 function calmath() {
-  document.getElementById("area-math").innerHTML = "<p>The area is: " + (7 * 12) + " cm²</p>"
-  document.getElementById("perimeter-math").innerHTML = "<p>The perimeter is: " + (7 * 2 + 12 * 2) + " cm</p>"
-      }
+  document.getElementById("area-math").innerHTML =
+    "<p>The area is: " + 7 * 12 + " cm²</p>"
+  document.getElementById("perimeter-math").innerHTML =
+    "<p>The perimeter is: " + (7 * 2 + 12 * 2) + " cm</p>"
+}
