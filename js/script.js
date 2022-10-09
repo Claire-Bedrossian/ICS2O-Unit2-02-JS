848aca66dbe4bf96bc028bfeac6b29b1802b5606
Claire-Bedrossi <113055648+Claire-Bedrossian@users.noreply.github.com>
2022-10-09 14:10:36 +0000

    Added Functions

---
+++ js/script.js
@@ -0,0 +1,4 @@
+function calmath() {
+  document.getElementById("area-math").innerHTML = "<p>The area is: " + (7 * 12) + " cm²</p>"
+  document.getElementById("perimeter-math").innerHTML = "<p>The perimeter is: " + (7 * 2 + 12 * 2) + " cm</p>"
+      }
